ae86c18ae4f1436cd5e64b93d4f72b122d44165c
Prakhar <prakharb56@gmail.com>
2023-05-02 17:50:23 +0530

    vite config

--- spotify-clone/vite.config.js
+++ spotify-clone/vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig, loadEnv } from 'vite'
-import { resolve } from 'path'
+import { defineConfig, loadEnv } from "vite";
+import { resolve } from "path";
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), "");
+  console.log(__dirname);
   return {
     // vite config
     define: {
@@ -13,12 +14,12 @@ export default defineConfig(({ command, mode }) => {
     build: {
       outDir: "../dist",
       rollupOptions: {
-          input: {
-              main: resolve(__dirname, 'src/index.html'),
-              dashboard: resolve(__dirname, 'src/dashboard/dashboard.html'),
-              login: resolve(__dirname, 'src/login/login.html')
-          }
-      }
-  }
-  }
-})
+        input: {
+          main: resolve(__dirname, "src/index.html"),
+          dashboard: resolve(__dirname, "src/dashboard/dashboard.html"),
+          login: resolve(__dirname, "src/login/login.html"),
+        },
+      },
+    },
+  };
+});
